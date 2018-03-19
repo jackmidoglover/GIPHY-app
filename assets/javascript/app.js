bee6a145dd2e8e597f4b378f8e7c79b5b11acb44
Jack <jackmidoglover@gmail.com>
2018-03-18 23:41:20 -0600

    mobile responsiveness added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,32 +5,6 @@ var faveAnim = [];
 
 $(document).ready(function(){
 
-function favoritesLoad() {
-    var storedStills = localStorage.getItem("still-list");
-    var faveStills2 = JSON.parse(storedStills);
-    var storedAnim = localStorage.getItem("anim-list");
-    var faveAnim2 = JSON.parse(storedAnim);
-    if (!faveStills2) {
-
-    }
-    else {
-    for (j = 0; j < faveStills2.length; j++) {
-    if (faveStills.includes(faveStills2[j])) {
-
-    }
-    else {
-    faveStills.push(faveStills2[j]);
-    }
-    if (faveAnim.includes(faveAnim2[j])) {
-
-    }
-    else {
-    faveAnim.push(faveAnim2[j]);
-    }
-}
-}
-}
-
 //primary function, calls GIPHY API and prints object info into cards
 function displayGif() {
     var clickCounter = $(this).attr("data-clicks") * 10;
@@ -129,60 +103,8 @@ $("#find-gif").on("click", function(event){
 
 // Makes topic buttons retrieve list of gifs
 $(document).on("click", ".gif", displayGif).on("click", ".gif", clicking);
-buttons();
-stillAnimate();
-favoritesLoad();
 
-// display gifs in favorites
-
-function displayFavorites() {
-
-    $(".favorites").css("visibility", "visible");
-    $(".card").css("z-index", "-1");
-
-    for (i = 0; i < faveStills.length; i++) {
-        var fave = $("<div>")
-        .addClass("frame");
-        var img = $("<img>")
-        img.addClass("faveGif")
-        .attr("src", faveStills[i])
-        .attr("anim-url", faveAnim[i])
-        .attr("still-url", faveStills[i])
-        .attr("isplaying", 'false');
-        $(fave).append(img);
-        $("#fave-gifs").prepend(fave);
-        console.log("works");
-    }
-    // make still gifs in favorite animate
-
-}
-function faveAnimate() {
-    $(document).on("click", ".faveGif", function() {
-        var moving = $(this).attr("isplaying");
-        if (moving === 'false') {
-        var srcURL = $(this).attr("anim-url");
-        $(this).attr("src", srcURL);
-        $(this).attr("isplaying", 'true');
-    }
-        else if (moving === 'true') {
-            var stillURL = $(this).attr("still-url");
-                $(this).attr("src", stillURL);
-                $(this).attr("isplaying", 'false');
-            }
-});
-}
-
-
-
-// Function to show/hide favorites window
-$("#faveWindow").on("click", displayFavorites).on("click", faveAnimate);
-$(".close").on("click", function(){
-    $(".favorites").css("visibility", "hidden");
-    $(".card").css("z-index", "0");
-    $("#fave-gifs").empty();
-})
-
-// Favorites button function
+// Add favorites button function
 $(document).on("click", ".fave", function(){
     event.preventDefault();
     var cardStill = $(this).parent().children("img").attr("still-url");
@@ -206,10 +128,90 @@ $(document).on("click", ".fave", function(){
     localStorage.setItem("anim-list", faveAnimString);
     console.log(localStorage.getItem("anim-list"));
     }
-
-
-
 })
+
+
+// Loads users favorites from local storage
+function favoritesLoad() {
+    var storedStills = localStorage.getItem("still-list");
+    var faveStills2 = JSON.parse(storedStills);
+    var storedAnim = localStorage.getItem("anim-list");
+    var faveAnim2 = JSON.parse(storedAnim);
+    if (!faveStills2) {
+
+    }
+    else {
+    for (j = 0; j < faveStills2.length; j++) {
+    if (faveStills.includes(faveStills2[j])) {
+
+    }
+    else {
+    faveStills.push(faveStills2[j]);
+    }
+    if (faveAnim.includes(faveAnim2[j])) {
+
+    }
+    else {
+    faveAnim.push(faveAnim2[j]);
+    }
+}
+}
+}
+
+// display favorites window and populate window with favorites
+
+function displayFavorites() {
+
+    $(".favorites").css("visibility", "visible");
+    $(".card").css("z-index", "-1");
+    faveAnimate();
+    for (i = 0; i < faveStills.length; i++) {
+        var fave = $("<div>")
+        .addClass("frame");
+        var img = $("<img>")
+        img.addClass("faveGif")
+        .attr("src", faveStills[i])
+        .attr("anim-url", faveAnim[i])
+        .attr("still-url", faveStills[i])
+        .attr("isplaying", 'false');
+        $(fave).append(img);
+        $("#fave-gifs").prepend(fave);
+    }
+
+// make still gifs in favorites animate
+
+}
+function faveAnimate() {
+    $(document).on("click", ".faveGif", function() {
+        var moving = $(this).attr("isplaying");
+        if (moving === 'false') {
+        var srcURL = $(this).attr("anim-url");
+        $(this).attr("src", srcURL);
+        $(this).attr("isplaying", 'true');
+    }
+        else if (moving === 'true') {
+            var stillURL = $(this).attr("still-url");
+                $(this).attr("src", stillURL);
+                $(this).attr("isplaying", 'false');
+            }
+});
+}
+
+
+
+// show/hide favorites window
+$("#faveWindow").on("click", displayFavorites);
+$(".close").on("click", function(){
+    $(".favorites").css("visibility", "hidden");
+    $(".card").css("z-index", "0");
+    $("#fave-gifs").empty();
+})
+
+//initializing functions on page load 
+buttons();
+stillAnimate();
+favoritesLoad();
+
 });
 
 
